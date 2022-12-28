2205109d48350927909dfa75ca57e315ea229e37
sanjeev2201 <sanjeevkumarrai132@gmail.com>
2022-12-28 20:24:56 +0530

    views.py updated

--- media_project/media_app/views.py
+++ media_project/media_app/views.py
@@ -3,11 +3,69 @@ from .forms import TeacherForm,StudentForm
 from .models import Teacher,Student
 import datetime
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 # Create your views here.
-def home(request):
+
+def index_page(request):
+    return render(request,'myapp/index.html')
+
+@login_required(login_url='index')#url name
+def home_page(request):
     return render(request,'myapp/base.html')
+
+#login request
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                print(user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("index")
+            else:
+                print('User not found')
+                messages.error(request,"No login credentials found")
+        else:
+            messages.error(request,"Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request,'myapp/login.html',{'form':form})
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']    
+            pass1=form.cleaned_data['password1']
+            pass2=form.cleaned_data['password2']
+            form.save()
+            user=auth.authenticate(username=username,password=pass1)
+            auth.login(request,user)
+            messages.info(request,'Account created successfully!!')
+            return redirect('login')
+    else:
+        form = SignUpForm()       
+    return render(request,'myapp/signup.html',{'form':form})
+
+def logout(request):
+        auth.logout(request)
+        messages.info(request, "You have successfully logged out.")
+        return  redirect('index')
     
-  
+def user_list(request):
+    users=User.objects.all()
+    return render(request,'myapp/user_list.html',{'users':users})
 def create_teacher(request):
     if request.method == 'POST':
         form = TeacherForm(request.POST,request.FILES)
